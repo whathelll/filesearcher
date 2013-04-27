2a5d061bf7fb90827ddc3d013597039844cbb357
whathelll <whathell@gmail.com>
2013-04-28 02:38:24 +1000

    icomlkj

--- filesearcher/index.js
+++ filesearcher/index.js
@@ -1,100 +1,137 @@
 var fs = require('fs');
 var path = require('path');
-var exec = require("child_process").exec;
+var exec = require('child_process').exec;
 
 var filesearcher = function() {
-	var option = {
-		"dir": "../",
-		"timeout": 10000,
-		"filepattern":  "log"
-	};
-	
-	this.setdir = function(newdir) {
-		option.dir = newdir;
-	}
-	this.getdir = function() {
-		return option.dir;
-	}
-	this.setfilepattern = function(pattern) {
-		option.filepattern = pattern;
-	}
-	
-	var filterbypattern = function(partname, files) {
-		for(var i = files.length-1; i >= 0; i--) {
-			if(!files[i].match(option.filepattern) || !files[i].match(partname)) {
-				files.splice(i, 1);
-			}
-		}
-		console.log('files after filtered out: ' + files);
-		return files;
-	}
-	
-	var grepforcontent = function(searchtext, callback) {
-		console.log('greping for: ' + searchtext);
-		exec('grep -l ' + searchtext + ' ' + option.dir + '*', {timeout: option.timeout}, function(err, stdout, stdin) {
-			if(err) {
-				console.log('error in grep: ' + err);
-			}
-			var results = stdout.split('\n');
-			results.pop();
-			console.log(results);
-			callback(results);
-		});
-	}
-	
-	var findfilesinpath = function(partname, searchtext, callback) {
-		console.log('starting search with partname:"' + partname + '" searchtext: ' + searchtext);
-		
-		fs.readdir(option.dir, function(err, files) {
-			if(err) {
-				console.log('error encountered reading dir: ' + err);
-				callback(null);
-			} else {
-				console.log('files found:' + files);
-				files = filterbypattern(partname, files);
-				callback(files);
-			}
-		});
-	}
-	
-	this.find = function(partname, searchtext, callback) {
-		console.log('looking in: ' + option.dir);
+    var option = {
+        "dir": __dirname,
+        "timeout": 10000,
+        "filepattern":  ""
+    };
 
-		fs.exists(option.dir, function(exists) {
-			if(exists) {
-				
-				if(searchtext && searchtext.length > 0) {
-					grepforcontent(searchtext, function(files) {
-						files = filterbypattern(partname, files);
-						
-						callback({"errors":null, "listoffiles":files});
-					});
-				} else {
-					findfilesinpath(partname, searchtext, function(files) {
-						callback({"errors":null, "listoffiles":files});
-					});
-				}
-			} else {
-				console.log('path does not exist:' + option.dir);
-				callback({"errors":'path does not exist', "listoffiles":null});
-			}
-		});
-		
-	}
-	
-	this.getfile = function(file, response) {
-		fs.exists(file, function(exists) {
-			if(exists) {
-				response.writeHead(200, {"Content-Type": "text/plain"});
-				var rs = fs.createReadStream(file);
-				rs.pipe(response);
-				
-			} else {
-				response.writeHead(200, {"Content-Type": "text/html"});
-				response.end("file not found");
-			}
-		});
-	}
+    this.setdir = function(newdir) {
+        option.dir = newdir;
+    }
+    this.getdir = function() {
+        return option.dir;
+    }
+    this.setfilepattern = function(pattern) {
+        option.filepattern = pattern;
+    }
+
+    var filterbypattern = function(partname, files) {
+        if(!partname) partname= "";
+        for(var i = files.length-1; i >= 0; i--) {
+            if(!files[i].match(option.filepattern) || !files[i].match(partname)) {
+                files.splice(i, 1);
+            }
+        }
+        console.log('files after filtered out: ' + files);
+        return files;
+    }
+
+    var grepforcontent = function(searchtext, callback) {
+        console.log('greping for: ' + searchtext);
+        exec('grep -l ' + searchtext + ' ' + option.dir + '*', {timeout: option.timeout}, function(err, stdout, stdin) {
+            if(err) {
+                console.log('error in grep: ' + err);
+                callback([]);
+            } else {
+                var results = stdout.split('\n');
+                results.pop(); //remove last one which is empty line
+
+                for(var i = 0; i < results.length; i++) {
+                    var file = results[i].split('/');
+                    results[i] = file.pop();
+                }
+                console.log(results);
+                callback(results);
+            }
+        });
+    }
+
+    var findfilesinpath = function(partname, searchtext, callback) {
+        console.log('starting search with partname:"' + partname + '" searchtext: ' + searchtext);
+
+        fs.readdir(option.dir, function(err, files) {
+            if(err) {
+                console.log('error encountered reading dir: ' + err);
+                callback(null);
+            } else {
+                console.log('files found:' + files);
+                files = filterbypattern(partname, files);
+                callback(files);
+            }
+        });
+    }
+
+    var getfileinfos = function(files, callback) {
+        var fileinfos = [];
+        if(!files || files.length === 0) {
+            callback(fileinfos)
+        };
+        files.forEach(function(file) {
+            fs.stat(option.dir + file, function(err, stat) {
+                if(stat && stat.isFile()) {
+                    //console.log('file stat:' + JSON.stringify(stat));
+                    fileinfos.push({
+                        "filename": file,
+                        "mtime": stat.mtime,
+                        "size": stat.size
+                    });
+
+                    //console.log('checking fileinfos length:' + fileinfos.length);
+                    if(fileinfos.length === files.length) {
+                        //console.log('returning fileinfos:' + JSON.stringify(fileinfos));
+                        callback(fileinfos);
+                    }
+                }
+            });
+
+        });
+    }
+
+    this.find = function(partname, searchtext, callback) {
+        console.log('looking in: ' + option.dir);
+
+        fs.exists(option.dir, function(exists) {
+            if(exists) {
+
+                if(searchtext && searchtext.length > 0) {
+                    grepforcontent(searchtext, function(files) {
+                        files = filterbypattern(partname, files);
+                        getfileinfos(files, function(files){
+                            callback({"errors":null, "listoffiles":files});
+                        });
+
+                    });
+                } else {
+                    findfilesinpath(partname, searchtext, function(files) {
+                        getfileinfos(files, function(files){
+                            callback({"errors":null, "listoffiles":files});
+                        });
+                    });
+                }
+            } else {
+                console.log('path does not exist:' + option.dir);
+                callback({"errors":'path does not exist', "listoffiles":null});
+            }
+        });
+    }
+
+    this.getfile = function(file, response) {
+        fs.exists(file, function(exists) {
+            if(exists) {
+                response.writeHead(200, {"Content-Type": "text/plain"});
+                var rs = fs.createReadStream(file);
+                rs.pipe(response);
+
+            } else {
+                response.writeHead(200, {"Content-Type": "text/html"});
+                response.end("file not found");
+            }
+        });
+    }
 }
 
 
--- server.js
+++ server.js
@@ -6,41 +6,51 @@ var filesearcher = require('./filesearcher');
 var fs = require('fs');
 
 function start() {
+    filesearcher.setdir(__dirname + '/logfiles/');
 
-	
-	//console.log(filesearcher);
-	//filesearcher.setdir('/xu/samba/will/node/server/logfiles/');
-	filesearcher.setdir(__dirname + '/logfiles/');
-	
-	function onRequest(request, response) {
-		var urlparts = url.parse(request.url, true);
-		var pathname = urlparts.pathname;
-		var query = urlparts.query;
-		logger.log("Request for " + pathname+ " received.");
-		logger.log("query string: " + JSON.stringify(query));
-		
-		if(pathname === "/logsearch") {
-			response.writeHead(200, {"Content-Type": "application/json"});
-			
-			filesearcher.find(query.filename, query.text, function(result){
-				response.end(JSON.stringify(result));
-			});
-		} else if(pathname === "/logsearch/" && pathname.indexOf("/logsearch/") === 0){
-			console.log('file requested:' + pathname);
-			
-			var file = filesearcher.getdir() + pathname.replace("/logsearch/", "");
-			
-			filesearcher.getfile(file, response);
-		} else {
-			response.writeHead(200, {"Content-Type": "text/html"});
-			response.write("something");
-			response.end();
-		}
-	}
+    function onRequest(request, response) {
+        var urlparts = url.parse(request.url, true);
+        var pathname = urlparts.pathname;
+        var query = urlparts.query;
+        logger.log("Request for " + pathname+ " received.");
+        logger.log("query string: " + JSON.stringify(query));
 
-	http.createServer(onRequest).listen(8080, "10.0.0.99");
+        if(pathname === "/logsearch") {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            var rs = fs.createReadStream(__dirname + '/app/index.html');
+            rs.pipe(response);
+        } else if(pathname === "/logquery") {
+            response.writeHead(200, {"Content-Type": "application/json"});
+            filesearcher.find(query.filename, query.text, function(result){
+                response.end(JSON.stringify(result));
+            });
+        } else if(pathname.indexOf("/logsearch/") === 0){
+            console.log('file requested:' + pathname);
 
-	logger.log('Server started running at 10.0.0.99:8080/');
+            var file = filesearcher.getdir() + pathname.replace("/logsearch/", "");
+
+            filesearcher.getfile(file, response);
+        } else if(pathname.indexOf('/css/') === 0 || pathname.indexOf('/js/') === 0) {
+            var file = __dirname + '/app' + pathname;
+            console.log('sending file:' + file);
+            if(/\.(css)$/.test(pathname)) {
+                response.writeHead(200, {"Content-Type": "text/css"});
+            } else if(/\.(js)$/.test(pathname)){
+                response.writeHead(200, {"Content-Type": "text/javascript"});
+            }
+
+            var rs = fs.createReadStream(file);
+            rs.pipe(response);
+        } else {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write("something");
+            response.end();
+        }
+    }
+
+    http.createServer(onRequest).listen(8080, "10.0.0.99");
+
+    logger.log('Server started running at 10.0.0.99:8080/');
 }
 
 exports.start = start;
